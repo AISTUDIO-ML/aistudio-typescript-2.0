ac2d3dde76ae7dbeb501ab591b800b02d65b4b34
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2023-10-14 22:08:20 +0500

    added pricing pate

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./components/style.css";
 import Login from "./components/login/Login";
-import Header from "./components/header/Header";
 import SignUp from "./components/signup/SignUp";
 import MoreSteps from "./components/signup/MoreSteps";
 import Hosting from "./components/hosting/Hosting";
+import PricingTable from "./components/pricingTable/PricingTable";
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/auth-steps" element={<MoreSteps />} />
           <Route path="/platform-usage" element={<Hosting />} />
+          <Route path="/checkout" element={<PricingTable />} />
         </Routes>
       </Router>
     </div>
